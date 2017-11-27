d6e59b823c8a9395a1bc2c8055342dc8b075e957
melvinjl <melvinjl@rose-hulman.edu>
2017-11-27 11:38:15 -0500

    m3 done

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -77,7 +77,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -88,10 +88,20 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+matt.right(90)
+matt.forward(50)
+matt.right(90)
+matt.forward(50)
+matt.right(90)
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
+matt.backward(50)
+matt.left(45)
+matt.forward(75)
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +119,20 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+timmy = rg.SimpleTurtle('circle')
+timmy.pen = rg.Pen('black', 30)
+timmy.forward(10)
+timmy.left(90)
+timmy.forward(50)
+timmy.right(90)
+timmy.forward(50)
+timmy.right(90)
+timmy.forward(50)
+timmy.right(90)
+timmy.forward(50)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
